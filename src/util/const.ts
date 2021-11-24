4cfadfded3586fa0871bb8a64efab38a4a96f391
xianeml <xianemml@gmail.com>
2021-11-24 22:54:25 +0900

    add: const 파일에 클래스명 상수 등록

---
+++ src/util/const.ts
@@ -0,0 +1,8 @@
+export default {
+  className: {
+    li: 'menu-list-item d-flex items-center py-2',
+    span: 'w-100 pl-2 menu-name',
+    editBtn: 'bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button',
+    removeBtn: 'bg-gray-50 text-gray-500 text-sm menu-remove-button',
+  },
+};
