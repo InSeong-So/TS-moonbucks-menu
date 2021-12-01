e5a5789ddd517d17fd300c7b4636ab4d43f90b03
InSeongSo <goflvhxj2547@gmail.com>
2021-12-01 23:20:06 +0900

    🗃Refactor: 공통 유틸 함수 추가 및 확장

---
+++ src/packages/common/index.ts
@@ -0,0 +1,116 @@
+export const $ = (selector: string) => {
+  return <HTMLElement>document.querySelector(selector);
+};
+
+export const $sibling = (
+  $element: HTMLElement,
+  parent: string,
+  targetSelector: string,
+) => {
+  return ($element.closest(parent) as HTMLElement).querySelector(
+    targetSelector,
+  );
+};
+
+export const isNull = (value: any) => value === null || value === undefined;
+
+export const getMaxLength = (target1 = 0, target2 = 1) => {
+  return Math.max(target1, target2);
+};
+
+export const isNotEquals = <T>(target1: T, target2: T) => {
+  return target1 !== target2;
+};
+
+export const isNodeNotEquals = (node1: HTMLElement, node2: HTMLElement) => {
+  return !node1.isEqualNode(node2);
+};
+
+export const isOnlyExistLeft = <T>(node1: T, node2: T) => {
+  return node1 && !node2;
+};
+
+export const isOnlyExistRight = <T>(node1: T, node2: T) => {
+  return !node1 && node2;
+};
+
+export const notationConvert = {
+  dashToCamel: (target: string) => {
+    return target.replace(/-[a-z]{1}/gi, char => {
+      return char.toUpperCase().substr(1);
+    });
+  },
+  snakeToCamel: (target: string) => {
+    return target.replace(/_[a-z]{1}/gi, char => {
+      return char.toUpperCase().substr(1);
+    });
+  },
+  camelToSnake: (target: string) => {
+    return target.replace(/([A-Z]{1})/g, '_$1').toLowerCase();
+  },
+};
+
+export const deepCloneToJSON = (state: object) => {
+  return JSON.parse(JSON.stringify(state));
+};
+
+export const isEqualsObject = (
+  object1: object | (() => void),
+  object2: object | (() => void),
+) => {
+  if (typeof object1 === 'function' || typeof object2 === 'function')
+    return object1.toString() === object2.toString();
+  return JSON.stringify(object1) === JSON.stringify(object2);
+};
+
+export const isCorrectType = (target: any, type: string) => {
+  return typeof target === type;
+};
+
+export const deepCloneAndFreeze = (state: object) => {
+  return Object.freeze(JSON.parse(JSON.stringify(state)));
+};
+
+export const debounceV1 = (callback: () => void) => {
+  let currentCallback: number | null = null;
+  return () => {
+    if (currentCallback) {
+      cancelAnimationFrame(currentCallback);
+      currentCallback = null;
+    }
+    currentCallback = requestAnimationFrame(() => callback());
+  };
+};
+
+export const debounce = (callback: any, wait = 0) => {
+  let currentArguments: object[] | undefined;
+  let timerId: number | undefined;
+
+  if (typeof callback !== 'function')
+    throw new TypeError('Expected a function');
+
+  const startTimer = (pendingCallback: () => any, term: number) => {
+    return setTimeout(pendingCallback, term);
+  };
+
+  const trailingEdge = () => {
+    timerId = undefined;
+    const args = <object[]>currentArguments;
+    currentArguments = undefined;
+    callback(...args);
+  };
+
+  return (...args: object[]) => {
+    currentArguments = args;
+    if (timerId === undefined) timerId = startTimer(trailingEdge, wait);
+  };
+};
+
+export const createUUID = () => {
+  let dt = new Date().getTime();
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, callback => {
+    const r = (dt + Math.random() * 16) % 16 | 0;
+    dt = Math.floor(dt / 16);
+    return (callback == 'x' ? r : (r & 0x3) | 0x8).toString(16);
+  });
+};
