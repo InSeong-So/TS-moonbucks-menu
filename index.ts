dc4b30dc99ebfedbcb4dc8fa13ef2615b78cd7f6
InSeongSo <goflvhxj2547@gmail.com>
2021-12-01 23:22:57 +0900

    ✒️Feat: 카테고리 라우터 구현

--- index.ts
+++ index.ts
@@ -1,3 +1,13 @@
-import App from './src/App.js';
+import appRoutes from './src/App.js';
+import Router from './src/packages/routes/index.js';
 
-const app = new App('#app');
+const router = new Router({
+  global: window,
+  tag: 'button',
+  dataset: 'category-name',
+});
+router
+  .addRoute('#/', appRoutes.home)
+  .addRoute('#/:category', appRoutes.menu)
+  .setNotFound(appRoutes.pageNotFound)
+  .start();
---
+++ src/packages/routes/index.ts
@@ -0,0 +1,105 @@
+import { notationConvert } from '../common/index.js';
+
+type HashRouteCallback = (params: string) => void;
+
+interface RoutesProps {
+  testRegExp: RegExp;
+  path: string;
+  targetComponent: HashRouteCallback;
+}
+
+interface RouterParams {
+  global: Window;
+  tag: 'button' | 'a';
+  dataset: string;
+}
+
+export default class Router {
+  $global: Window;
+  $routeSelector: string;
+  routes: RoutesProps[];
+  dataset: string;
+
+  constructor({ global, tag, dataset }: RouterParams) {
+    this.$global = global;
+    this.$routeSelector = `${tag}[data-${dataset}]`;
+    this.routes = [];
+    this.dataset = notationConvert.dashToCamel(dataset);
+  }
+
+  pageNotFound = (hash: string) => {
+    console.error(`${hash} Page Not Fount`);
+  };
+
+  checkRoutes = () => {
+    const { hash } = this.$global.location;
+    const currentRoute = this.routes.find(({ testRegExp }) => {
+      return testRegExp.test(hash);
+    });
+
+    if (!currentRoute) return this.pageNotFound(hash);
+
+    const selected = hash.substring(2);
+
+    this.setActive(selected);
+
+    currentRoute.targetComponent(selected);
+  };
+
+  addRoute = (path: string, targetComponent: HashRouteCallback) => {
+    const ROUTE_PARAMETER_REGEXP = /:(\w+)/g;
+    const URL_FRAGMENT_REGEXP = '([^\\/]+)';
+
+    const params = [];
+
+    const parsedFragment = path
+      .replace(ROUTE_PARAMETER_REGEXP, (_, paramName) => {
+        params.push(paramName);
+        return URL_FRAGMENT_REGEXP;
+      })
+      .replace(/\//g, '\\/');
+
+    this.routes.push({
+      testRegExp: new RegExp(`^${parsedFragment}$`),
+      path,
+      targetComponent,
+    });
+
+    return this;
+  };
+
+  setNotFound = (callback: HashRouteCallback) => {
+    this.pageNotFound = callback;
+    return this;
+  };
+
+  navigate = (path: string) => {
+    location.hash = path;
+  };
+
+  setActive = (route: string) => {
+    // 이 내용 공유하려고 합니다!!
+    const $$a = <NodeListOf<HTMLElement>>(
+      document.querySelectorAll('.cafe-category-name')
+    );
+    $$a.forEach(link => {
+      if (link.dataset[this.dataset] === route) link.classList.add('active');
+      else link.classList.remove('active');
+    });
+  };
+
+  start = () => {
+    window.addEventListener('click', (event: MouseEvent) => {
+      const target = <HTMLButtonElement>event.target;
+      if (!target.matches(this.$routeSelector)) return;
+      event.preventDefault();
+      const route = target.dataset[this.dataset];
+      this.navigate(`/${route}`);
+    });
+
+    if (!this.$global.location.hash) this.$global.location.hash = '#/espresso';
+    this.checkRoutes();
+
+    this.$global.addEventListener('hashchange', this.checkRoutes);
+  };
+}
