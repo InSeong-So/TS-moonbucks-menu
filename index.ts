ff8a74880c043ff756b7e99a5da95f70b95c053f
InSeongSo <goflvhxj2547@gmail.com>
2022-01-06 14:04:41 +0900

    ✒️Feat: App 초기 세팅을 위한 상수 추가 및 엔트리 코드 정리

--- index.ts
+++ index.ts
@@ -1,14 +1,23 @@
 import './assets/css/index.scss';
 
-import vdom from './src/App';
+import App from './src/App';
 import render from './src/helpers/dom/render';
-import Observer from './src/observer';
+import store from './src/store';
+import { CATEGORIES } from './src/constants';
 
-const observer = new Observer();
 const diffRenderer = () => {
-  render(document.getElementById('app') as HTMLElement, vdom);
+  const $root = document.getElementById('app') as HTMLElement;
+  const $old = $root.firstChild as HTMLElement;
+  const $new = App(store);
+  render($root, $old, $new);
 };
 
-observer.subscribe(render);
+// 초기값 설정
+store.create('categories', CATEGORIES);
+store.create('current', { id: 'espresso', text: '☕ 에스프레소' });
 
+// 구독
+store.subscribe(diffRenderer);
+
+// 최초 실행
 diffRenderer();
