a9afb9033cea35093079790775b63fe941489014
InSeongSo <goflvhxj2547@gmail.com>
2022-01-06 14:06:14 +0900

    ✒️Feat: UI 컴포넌트 수정

--- src/components/header/Header.ts
+++ src/components/header/Header.ts
@@ -1,25 +1,21 @@
-const Header = () => {
+import { TPresentation, TProps } from 'Component';
+
+const Header: TPresentation = ({ categories }) => {
   return `
   <header class="my-4">
     <a href="/" class="text-black">
       <h1 class="text-center font-bold">🌝 문벅스 메뉴 관리</h1>
     </a>
     <nav class="d-flex justify-center flex-wrap">
-      <button data-category-name="espresso" class="cafe-category-name btn bg-white shadow mx-1">
-        ☕ 에스프레소
-      </button>
-      <button data-category-name="frappuccino" class="cafe-category-name btn bg-white shadow mx-1">
-        🥤 프라푸치노
-      </button>
-      <button data-category-name="blended" class="cafe-category-name btn bg-white shadow mx-1">
-        🍹 블렌디드
-      </button>
-      <button data-category-name="teavana" class="cafe-category-name btn bg-white shadow mx-1">
-        🫖 티바나
-      </button>
-      <button data-category-name="desert" class="cafe-category-name btn bg-white shadow mx-1">
-        🍰 디저트
-      </button>
+      ${categories
+        .map((category: TProps) => {
+          return `
+        <button data-category-name="${category.id}" class="cafe-category-name btn bg-white shadow mx-1">
+          ${category.text}
+        </button>
+        `;
+        })
+        .join('')}
     </nav>
   </header>
   `;
