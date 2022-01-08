6723f7c254faad9a220077d91109291c418f1351
sinholee <dltlsgh5@gmail.com>
2022-01-09 01:38:08 +0900

    Revert "Revert "[feature] http client 추상화 1""
    This reverts commit 8c27b6e76c89d6ed8e063f0d0d730ab593c29f6f.

--- src/modules/notUsingMiddlewares/Repository.ts
+++ src/modules/notUsingMiddlewares/Repository.ts
@@ -42,55 +42,60 @@ export interface CurrentMenuRepository {
 }
 
 export const createCurrentMenuRepository = (() => {
+  let repository: CurrentMenuRepository;
   return (
     store: ReturnCreateStore<DefaultState, Actions>,
     db: any,
   ): CurrentMenuRepository => {
     const { dispatch, getState } = store;
-    return {
-      currentTab: () => {
-        const currentState = getState();
-        return Coffee[currentState.currentTab];
-      },
-      getList: () => {
-        const currentState = getState();
-        return currentState.menus[currentState.currentTab];
-      },
-      findById: (id: string | undefined) => {
-        return createCurrentMenuRepository(store, db)
-          .getList()
-          .find(coffee => coffee.id === id);
-      },
-      findByText: (text: string) => {
-        return createCurrentMenuRepository(store, db)
-          .getList()
-          .find(coffee => coffee.text === text);
-      },
-      remove: (menuId: string) => {
-        // db action, state action
-        dispatch(removeMenu(menuId));
-      },
-      changeTab: (selectedTab: CoffeeKeys) => {
-        dispatch(changeTab(selectedTab));
-      },
-      toggleSoldOut: (menuId: string) => {
-        // http.client
-        dispatch(soldOutMenu(menuId));
-      },
-      edit: async ({ menuId, text, category }) => {
-        // http.client put
-        const newMenu = await menuClient.editText({ menuId, text, category });
-        dispatch(
-          editMenu({
-            ...adjustMenuItem(newMenu),
-          }),
-        );
-      },
-      add: async (text, category) => {
-        const menuItem = await menuClient.add(text, category);
-        console.log(menuItem);
-        dispatch(addMenu(adjustMenuItem(menuItem)));
-      },
-    };
+    if (!repository) {
+      repository = {
+        currentTab: () => {
+          const currentState = getState();
+          return Coffee[currentState.currentTab];
+        },
+        getList: () => {
+          const currentState = getState();
+          return currentState.menus[currentState.currentTab];
+        },
+        findById: (id: string | undefined) => {
+          return createCurrentMenuRepository(store, db)
+            .getList()
+            .find(coffee => coffee.id === id);
+        },
+        findByText: (text: string) => {
+          return createCurrentMenuRepository(store, db)
+            .getList()
+            .find(coffee => coffee.text === text);
+        },
+        remove: (menuId: string) => {
+          // db action, state action
+          dispatch(removeMenu(menuId));
+        },
+        changeTab: (selectedTab: CoffeeKeys) => {
+          dispatch(changeTab(selectedTab));
+        },
+        toggleSoldOut: (menuId: string) => {
+          // http.client
+          dispatch(soldOutMenu(menuId));
+        },
+        edit: async ({ menuId, text, category }) => {
+          // http.client put
+          const newMenu = await menuClient.editText({ menuId, text, category });
+          console.log(newMenu);
+          dispatch(
+            editMenu({
+              ...adjustMenuItem(newMenu),
+            }),
+          );
+        },
+        add: async (text, category) => {
+          const menuItem = await menuClient.add(text, category);
+
+          dispatch(addMenu(adjustMenuItem(menuItem)));
+        },
+      };
+    }
+    return repository;
   };
 })();
