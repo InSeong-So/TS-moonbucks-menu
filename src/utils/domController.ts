1e3fea2b03927930aaa32702d2f727c5c6d3a2e1
xianeml <xianemml@gmail.com>
2022-01-19 23:18:55 +0900

    modify: domControllers 불필요한 함수 삭제

--- src/utils/domController.ts
+++ src/utils/domController.ts
@@ -1,27 +1,11 @@
 export const $ = (selector: string) => {
-  return <HTMLElement>document.querySelector(selector);
+  return document.querySelector(selector) as HTMLElement;
 };
 
-type ElCreationOptions = {
-  elName: string;
-  id?: string;
-  className?: string;
-  text?: string;
-};
-
-export const addElement = (options: ElCreationOptions) => {
-  const { elName, id, className, text } = options;
-  const $el = document.createElement(elName);
-  $el.id = id || '';
-  $el.className = className || '';
-  $el.textContent = text || '';
-  return $el;
-};
-
-export const bindEvent = (
-  $el: HTMLElement,
-  eventName: string,
-  handler: () => void,
-) => {
-  $el.addEventListener(eventName, handler);
-};
+// export const bindEvent = (
+//   $el: HTMLElement,
+//   eventName: string,
+//   handler: (e: Event) => void,
+// ) => {
+//   $el.addEventListener(eventName, handler);
+// };
